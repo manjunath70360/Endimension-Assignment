5139503783a07ba27cc0348f83e8f7f4142c4f30
Manjunath <122592837+manjunath70360@users.noreply.github.com>
2024-05-15 11:13:23 +0530

    Update index.js

--- src/components/AddProductForm/index.js
+++ src/components/AddProductForm/index.js
@@ -9,23 +9,24 @@ const { Option } = Select;
 
 const ProductForm = ({ history, match }) => {
   const { products, addProduct, editProduct } = useContext(ProductContext);
-  const [product, setProduct] = useState({ category: '', name: '', description: '', price: '' });
+  const [form] = Form.useForm();
   const productId = match.params.id;
 
   useEffect(() => {
     if (productId) {
       const existingProduct = products.find((p) => p.id === Number(productId));
       if (existingProduct) {
-        setProduct(existingProduct);
+        form.setFieldsValue(existingProduct);
       }
     }
-  }, [productId, products]);
+  }, [productId, products, form]);
 
   const onFinish = (values) => {
     if (productId) {
       editProduct(Number(productId), { ...values, id: Number(productId) });
     } else {
-      addProduct({ ...values, id: Date.now() });
+      const newProduct = { ...values, id: products.length + 1 }; 
+      addProduct(newProduct);
     }
     history.push('/');
   };
@@ -33,7 +34,12 @@ const ProductForm = ({ history, match }) => {
   return (
     <div className='product-form-con'>
       <h1 className='head'>{productId ? 'Edit Product' : 'Add Product'}</h1>
-      <Form layout="vertical" onFinish={onFinish} initialValues={product}>
+      <Form
+        form={form}
+        layout="vertical"
+        onFinish={onFinish}
+        initialValues={{ category: '', name: '', description: '', price: '' }}
+      >
         <Form.Item
           name="category"
           label="Category"
@@ -44,7 +50,7 @@ const ProductForm = ({ history, match }) => {
             <Option className='select-field' value="Electronics">Electronics</Option>
             <Option className='select-field' value="Clothing">Clothing</Option>
             <Option className='select-field' value="Books">Books</Option>
-            {/* Add more options as needed */}
+            
           </Select>
         </Form.Item>
         <Form.Item
@@ -69,7 +75,7 @@ const ProductForm = ({ history, match }) => {
           className='label'
           rules={[{ required: true, message: 'Please enter the product price!' }]}
         >
-          <Input type="number" className='input-field' placeholder="Enter the product price" />
+          <Input className='input-field' type="number" placeholder="Enter the product price" />
         </Form.Item>
         <Form.Item>
           <Button className='button' type="primary" htmlType="submit">
